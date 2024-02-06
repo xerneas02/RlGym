2c0fdf5d79ccc26a1d5d9da55ffdb64ef4a6427f
mynoteet <mynoteet@gmail.com>
2024-02-06 12:07:49 +0100

    stable_version

--- RlGym.py
+++ RlGym.py
@@ -9,7 +9,6 @@ from rlgym.utils.gamestates import PlayerData, GameState
 from rlgym.utils.reward_functions import RewardFunction
 from rlgym.utils.common_values import BALL_RADIUS, CAR_MAX_SPEED, BALL_MAX_SPEED
 from numpy.linalg import norm
-<<<<<<< HEAD
 from Observer import ObsBuilderBluePerspective
 from Reward import CustomReward
 from Terminal import BallTouchCondition
@@ -24,58 +23,6 @@ env = rlgym.make(
     reward_fn=CustomReward(), 
     state_setter=StateSetterInit(),
     obs_builder=ObsBuilderBluePerspective())
-=======
-
-class BallTouchCondition(TerminalCondition):
-    
-    def __init__(self):
-        super().__init__()
-        self.last_touch = None
-
-    def reset(self, initial_state: GameState):
-        self.last_touch = initial_state.last_touch
-
-    def is_terminal(self, current_state: GameState) -> bool:
-        return current_state.last_touch != self.last_touch
-
-
-class CustomReward(RewardFunction):
-    def __init__(self):
-        super().__init__()
-        self.last_touch = None
-
-    def reset(self, initial_state: GameState):
-        self.last_touch = initial_state.last_touch
-
-    def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
-        dist_ball = np.linalg.norm(player.car_data.position - state.ball.position) - BALL_RADIUS
-        dist_reward = np.exp(-0.5 * dist_ball / CAR_MAX_SPEED)
-        ball_speed = norm(state.ball.linear_velocity)
-
-        total_reward = dist_reward + player.on_ground*0.3 + 2*ball_speed/BALL_MAX_SPEED + player.match_goals*10
-
-        return total_reward
-
-    def get_final_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
-        return self.get_reward(player, state, previous_action)
-
-
-
-env = rlgym.make(game_speed=100, terminal_conditions=(common_conditions.TimeoutCondition(225), common_conditions.GoalScoredCondition()), reward_fn=CustomReward())
->>>>>>> ab43d8ba10a53d41218b4a374283b2457290a801
-
-class CustomPolicy(nn.Module):
-    def __init__(self, obs_dim, act_dim):
-        super(CustomPolicy, self).__init__()
-        self.mlp_extractor = model.policy.mlp_extractor
-        self.action_net = model.policy.action_net
-        self.value_net = model.policy.value_net
-
-    def forward(self, x):
-        features = self.mlp_extractor(x)
-        action_out = self.action_net(features)
-        value_out = self.value_net(features)
-        return action_out, value_out
 
 model = PPO.load("rl_model")
 model.set_env(env)
