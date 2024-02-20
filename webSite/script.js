479fa04e5c4f092e2a0270234dd5f1e03ca67c26
xerneas02 <mathis.roubille@gmail.com>
2024-02-20 18:45:07 +0100

    animeation marche a peut près

--- webSite/script.js
+++ webSite/script.js
@@ -7,9 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (toggleContent.style.maxHeight) {
                 toggleContent.style.maxHeight = null;
+                toggleContent.style.overflow = 'hidden'; // Masquer le contenu qui dépasse lors de la fermeture
                 toggleTitle.querySelector('.toggle-arrow').textContent = '🡆';
             } else {
                 toggleContent.style.maxHeight = toggleContent.scrollHeight + 'px';
+                toggleContent.style.overflow = 'visible'; // Utiliser 'visible' lorsque le contenu est ouvert
                 toggleTitle.querySelector('.toggle-arrow').textContent = '🡇';
             }
         });
@@ -17,6 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
         // Afficher le contenu par défaut au chargement de la page
         var toggleContent = toggleTitle.nextElementSibling;
         toggleContent.style.maxHeight = toggleContent.scrollHeight + 'px';
+        toggleContent.style.overflow = 'visible'; // Assurez-vous que le défilement est activé par défaut
         toggleTitle.querySelector('.toggle-arrow').textContent = '🡇';
+
+        // Détecter la fin de l'animation de fermeture et modifier le mode d'overflow
+        toggleContent.addEventListener('transitionend', function() {
+            if (!toggleContent.style.maxHeight) {
+                toggleContent.style.overflow = 'hidden'; // Rétablir le mode d'overflow à 'hidden' lorsque l'animation est terminée
+            }
+        });
     });
 });
